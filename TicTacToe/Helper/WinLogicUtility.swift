7e029c5eebc879360a3c48925b3a46608a77bde7
Parth Contractor <contractor110@gmail.com>
2019-09-19 21:06:18 +0530

    WinLogicUtility refactoring

--- TicTacToe/Helper/WinLogicUtility.swift
+++ TicTacToe/Helper/WinLogicUtility.swift
@@ -11,37 +11,49 @@ import Foundation
 struct WinLogicUtility{
     static func checkIfPlayerWins(_ position: Position) -> Bool{
         //Row check
-        let fullRowSelectedByThisPlayer = TicTacToeStore.boardMappingArray[position.row].allSatisfy { $0 == position.player!.rawValue }
-        if fullRowSelectedByThisPlayer {
+        if fullRowSelectedByThisPlayer(position) {
             return true
         }
         
         //Column check
-        let fullColumnSelectedByThisPlayer = TicTacToeStore.boardMappingArray[column: position.column].allSatisfy { $0 == position.player!.rawValue }
-        if fullColumnSelectedByThisPlayer {
+        if fullColumnSelectedByThisPlayer(position) {
             return true
         }
         
         //Primary diagonal.. check
-        var primaryDiagonalArray = [Int]()
-        for index in 0..<numberOfSquares {
-            primaryDiagonalArray.append(TicTacToeStore.boardMappingArray[index][index])
-        }
-        let fullPrimaryDiagonalSelectedByThisPlayer = primaryDiagonalArray.allSatisfy { $0 == position.player!.rawValue }
-        if fullPrimaryDiagonalSelectedByThisPlayer {
+        if fullPrimaryDiagonalSelectedByThisPlayer(position) {
             return true
         }
         
         //Anti-diagonal.. check
-        var antiDiagonalArray = [Int]()
-        for index in 0..<numberOfSquares {
-            antiDiagonalArray.append(TicTacToeStore.boardMappingArray[index][numberOfSquares - index - 1])
-        }
-        let fullAntiDiagonalSelectedByThisPlayer = antiDiagonalArray.allSatisfy { $0 == position.player!.rawValue }
-        if fullAntiDiagonalSelectedByThisPlayer {
+        if fullAntiDiagonalSelectedByThisPlayer(position) {
             return true
         }
         
         return false
+    }
+    
+    private static func fullRowSelectedByThisPlayer(_ position: Position) -> Bool {
+        return TicTacToeStore.boardMappingArray[position.row].allSatisfy { $0 == position.player!.rawValue }
+    }
+    
+    private static func fullColumnSelectedByThisPlayer(_ position: Position) -> Bool {
+        return TicTacToeStore.boardMappingArray[column: position.column].allSatisfy { $0 == position.player!.rawValue }
+    }
+    
+    private static func fullPrimaryDiagonalSelectedByThisPlayer(_ position: Position) -> Bool {
+        var primaryDiagonalArray = [Int]()
+        for index in 0..<numberOfSquares {
+            primaryDiagonalArray.append(TicTacToeStore.boardMappingArray[index][index])
+        }
+        return primaryDiagonalArray.allSatisfy { $0 == position.player!.rawValue }
+    }
+    
+    private static func fullAntiDiagonalSelectedByThisPlayer(_ position: Position) -> Bool {
+        var antiDiagonalArray = [Int]()
+        for index in 0..<numberOfSquares {
+            antiDiagonalArray.append(TicTacToeStore.boardMappingArray[index][numberOfSquares - index - 1])
+        }
+        return antiDiagonalArray.allSatisfy { $0 == position.player!.rawValue }
     }
 }
